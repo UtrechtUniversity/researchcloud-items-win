268c46417d80d33880a41695ff2f4430b750c6c3
Dawa Ometto <d.l.a.ometto1@uu.nl>
2024-08-06 13:59:35 +0200

    test

--- scripts/lib/debug.ps1
+++ scripts/lib/debug.ps1
@@ -1,17 +1,23 @@
-if (Get-Command 'Get-ExecutionPolicy' -errorAction SilentlyContinue) {
-    Write-Output 'Get-ExecutionPolicy exists'
-} else {
-    Write-Output 'Get-ExecutionPolicy does not exist'
-}
-
 $result = Get-Command 'Get-ExecutionPolicy'
 Write-Output "Get-Command $result"
 
+$result = cat "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\Microsoft.PowerShell.Security\Microsoft.PowerShell.Security.psd1"
+Write-Output "cat $result"
+
+$result = Import-Module PowerShellGet
+Write-Output "Import-Module $result"
+
 $result = Get-InstalledModule
-Write-Output "Get-Installed Module $result"
+Write-Output "Get-Installed Modules $result"
+
+$result = Get-Module -List
+Write-Output "Get-Installed Modules 2 $result"
+
+$result = Import-Module Microsoft.PowerShell.Security
+Write-Output "Import-Module $result"
 
 $result = Get-Module 'Microsoft.PowerShell.Security'
 Write-Output "Get-Module $result"
 
-$result = cat "C:\Windows\system32\WindowsPowerShell\v1.0\Modules\Microsoft.PowerShell.Security\Microsoft.PowerShell.Security.psd1"
-Write-Output $result
+$result = Get-Command 'Get-ExecutionPolicy'
+Write-Output "Get-Command $result"
